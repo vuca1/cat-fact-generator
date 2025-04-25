aed2766822da1b50527dcdfde4b9dd4b22bdd8e0
vuca1 <malihavita@gmail.com>
2025-04-25 07:59:48 +0200

    Save facts to files function added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+#gets cat facts from API
 def get_cat_fact():
     try:
         response = requests.get("https://catfact.ninja/fact") #gets the fact
@@ -8,6 +9,16 @@ def get_cat_fact():
         return data["fact"] #returns only the fact from the response (dictionary)
     except requests.exceptions.RequestException as e:
         return f"Error: {e}"
+
+#saves facts into text file
+def save_to_file(facts, filename = "cat_facts.txt"):
+    try:
+        with open(filename, "w", encoding = "utf-8") as file:
+            for i in facts:
+                file.write(i + "\n")
+            print(f"Saved {len(facts)} cat facts to file: {filename}")
+    except Exception as e:
+        print(f"Error occurred while saving {filename}: {e}")
 
 def main():
     print("Welcome to Cat Fact Generator!")
@@ -28,8 +39,15 @@ def main():
     print("\nHere are your cat facts:\n")
     for i in facts:
         print(i)
+    print()
+
+    #asks user if to save to files
+    do_save = input("Would you like to save the facts into file? [yes/no]")
+    if do_save in ["yes", "y"]:
+        save_to_file(facts)
 
 #only run the main() function if this file is being run directly — not if it's being imported somewhere else
+#always use 'main() guard' to prevent unwanted run while imported
 if __name__ == "__main__":
     main()
 
